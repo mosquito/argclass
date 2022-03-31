a7aac5ad635e9f4073e7d7804f0ea60539e116e5
Dmitry Orlov <me@mosquito.su>
2022-04-01 00:05:43 +0300

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="argclass",
-    version="0.2.0",
+    version="0.2.1",
     platforms="all",
     author="Dmitry Orlov",
     author_email="me@mosquito.su",
